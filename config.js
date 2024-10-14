9101414e6312509241c07b4fdd432b70fc1e3cd6
dungvv_home <dungvv@taap.vn>
2024-10-14 23:43:40 +0700

    lonchao

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'mừng ngày sn bé tố trân ',
+    introDesc: 'Dean Trbl có bức thư gửi bé',
+    btnIntro: 'mở luôn cho nóng',
+    title: 'bé thích quà gì nào',
+    desc: 'chọn cho chuẩn zô, không chọn là chó',
+    btnYes: 'ko cần quà âu chỉ cần chảo vui vẻ thui',
+    btnNo: 'Adidas Samba Wales Bonner Metallic Silver',
+    question:'quàooo (⁄ ⁄>⁄ ▽ ⁄<⁄ ⁄) điều gì bé thích ở Chảo nhất nào?',
+    btnReply: 'yêu nhắm ♡ (￣З￣)',
+    reply: 'xinh gái đáng yêu tâm lí ❤',
+    mess: 'mê ( • )( • )ԅ(≖‿≖ԅ) lun',
+    messDesc: '❤ giỏi nhắm, giờ là món quà thứ 2 cho bé nè, zô chọn mẫu ik (ɔˆз(ˆ⌣ˆc)',
+    btnAccept: 'Adidas Samba Wales Bonner Metallic Silver',
+    messLink: 'https://vt.tiktok.com/ZS2KhsgtS/'
+}
